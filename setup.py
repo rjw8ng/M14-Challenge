cb6cf237cd7c2c8f94f7fd973d223f89c6b7498f
rjw8ng <107445344+rjw8ng@users.noreply.github.com>
2022-07-11 10:33:48 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/rjw8ng/M14-Challenge',
+      author='Royal Wang',
+      author_email='rjw8ng@virginia.edu',
+      license='MIT',
+      packages=['demo'])
